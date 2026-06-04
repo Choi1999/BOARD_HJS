87c8af89a2a085d467cdbad2bdd5320b67bb24a0
admin1 <admin@admin.com>
2026-06-04 14:30:02 +0900

    first commit

---
+++ board_dao.py
@@ -0,0 +1,36 @@
+import pymysql
+
+class BoardDAO:
+
+    def init(self):
+        self.host ="localhost"
+        self.user ="board_user"
+        self.port= 3306
+        self.password ="board1234"
+        self.database ="board_db"
+
+    def get_connection(self):
+        return pymysql.connect(
+            host="localhost",
+            user="board_user",
+            password="board1234",
+            database="board_db",
+            charset="utf8mb4" # 
+        )
+
+    def select_all(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        SELECT *
+        FROM board
+        ORDER BY id DESC
+        """
+
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        return result
